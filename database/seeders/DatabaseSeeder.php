8f2096f7678d134d26b98124e5f3d039e3311133
joseias <joseias@gmail.com>
2025-03-04 08:13:18 +0100

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-use Database\Seeders\CategoriesSeeder;
 use Database\Seeders\UserSeeder;
 use Database\Seeders\PhoneSeeder;
 use Database\Seeders\PostSeeder;
@@ -18,8 +17,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CategoriesSeeder::class);
-        $this->call(ProductSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(PhoneSeeder::class);
         $this->call(PostSeeder::class);
